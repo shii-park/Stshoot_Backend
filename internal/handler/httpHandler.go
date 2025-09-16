d3451d54be91e51752e288ce3985ca58a7fc4d19
meg44k <yugomegane@gmail.com>
2025-09-17 00:31:24 +0900

    Add: /createの部屋作成ハンドラを実装しました

--- internal/handler/httpHandler.go
+++ internal/handler/httpHandler.go
@@ -1,1 +1,19 @@
 package handler
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/shii-park/Stshoot_Backend/internal/broadcaster"
+)
+
+func HandleCreate(w http.ResponseWriter, r *http.Request, hubManager *broadcaster.HubManager) {
+	roomID, err := hubManager.CreateHub()
+	if err != nil {
+		http.Error(w, "Failed to create hub", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(map[string]string{"roomID": roomID})
+}
