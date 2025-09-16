b053224ee036e8d285d6352bb133487f40b87ab9
meg44k <yugomegane@gmail.com>
2025-09-16 15:51:16 +0900

    Add: WebSocketの基礎となる機能を実装しました

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
 
+	"github.com/shii-park/Stshoot_Backend/internal/handler"
+)
+
+func main() {
+	http.HandleFunc("/ws", handler.HandleWebSocket)
+	fmt.Println("WebSocket server started on ws://localhost:8080/ws")
+	http.ListenAndServe(":8080", nil)
 }
--- internal/handler/websocket_handler.go
+++ internal/handler/websocket_handler.go
@@ -1,1 +1,37 @@
 package handler
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+func HandleWebSocket(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Println("WebSocket connection failed:", err)
+		return
+	}
+
+	go handleConnection(conn) //一つのコネクションに対してゴルーチンで並列にあつかう
+}
+
+func handleConnection(conn *websocket.Conn) {
+	defer conn.Close()
+
+	welcomeMessage := []byte("サーバーへの接続に成功しました！")
+	println("Someone connected")
+
+	if err := conn.WriteMessage(websocket.TextMessage, welcomeMessage); err != nil {
+
+		fmt.Println("Failed to write welcome message:", err)
+		return
+	}
+}
