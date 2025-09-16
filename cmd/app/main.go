b270b6d0f96edc00ce01d4c4f414db58497abfa7
meg44k <yugomegane@gmail.com>
2025-09-16 20:11:04 +0900

    Fix: ルーティングに対するハンドラを追加しました

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -4,11 +4,19 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/shii-park/Stshoot_Backend/internal/broadcaster"
 	"github.com/shii-park/Stshoot_Backend/internal/handler"
 )
 
 func main() {
-	http.HandleFunc("/ws", handler.HandleWebSocket)
-	fmt.Println("WebSocket server started on ws://localhost:8080/ws")
+
+	receiver := &broadcaster.Receiver{}
+
+	hub := broadcaster.NewHub(receiver)
+	go hub.Run()
+
+	http.HandleFunc("/ws/receiver", func(w http.ResponseWriter, r *http.Request) { handler.HandleReceiver(w, r, receiver) })
+	http.HandleFunc("/ws/sender", func(w http.ResponseWriter, r *http.Request) { handler.HandleSender(w, r, hub) })
+	fmt.Println("WebSocket server started on ws://localhost:80/ws")
 	http.ListenAndServe(":80", nil)
 }
